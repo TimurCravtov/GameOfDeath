184c822b6b2b9ee505e91ac99f87e7e5d9ed1d91
Timur Cravtov <timurcravtov04@gmail.com>
2025-03-24 16:55:14 +0200

    added some assets

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public int width = 20;
+    public int height = 20;
+
+
+    void Start()
+    {
+     
+    }
+
+    void InitializeGrid()
+    {
+      
+    }
+
+    void UpdateCellVisual(int x, int y)
+    {
+       
+    }
+}
